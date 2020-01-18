27e43af69979e574125864ccf36dab006736fbcc
corrado4eyes <corrado@splendo.com>
2020-01-18 17:11:59 +0100

    [#2] Imported reflect-metadata in index.tsx

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from '../utils/serviceWorker';
+import 'reflect-metadata';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
